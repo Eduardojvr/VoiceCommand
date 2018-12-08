aa78b5adb76064b60de0e3e56fe686c77bb52b01
Eduardojvr <eduardojvr10@gmail.com>
2018-12-08 12:42:04 -0200

    Adicionando funcionalidade de leitura de textos presentes em imagens

--- command.py
+++ command.py
@@ -9,9 +9,25 @@ import time
 import os
 import sys
 import webbrowser
+from PIL import Image
+import pytesseract as pyt
+import cv2
 
 speak = pyttsx3.init()
 estado=0
+
+
+def conversor(arquivo):
+    imagem = cv2.imread(arquivo)
+    filenameImagem = "{}.png".format(os.getpid())
+    cv2.imwrite(filenameImagem, imagem) 
+    texto = pyt.image_to_string(Image.open(filenameImagem))
+    os.remove(filenameImagem)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows() 
+    fala("Iniciando a leitura.")  
+    fala(texto)
+    fala("Fim da leitura!")
 
 
 def fala(text):
@@ -33,21 +49,29 @@ def comandos(cmd):
             webbrowser.open_new("https://www.youtube.com")
         elif cmd=="studio":
             os.popen('cd / && cd Applications && open Visual\ Studio\ Code.app/') 
+        elif cmd=="text":
+            fala("Você escolheu a opção de leitura de texto em imagens!")
+            fala("Insira aqui o caminho da imagem juntamente com o seu nome e extensão!")
+            caminho = input('Digite:')
+            caminho = str(caminho)
+            conversor(caminho)
         else:
-            fala("Não existe esse comando na minha lista de permissões!")
+            fala("Não existe esse comando na minha lista!")
     else: 
         return 1
 
+bot = "Olá, meu nome é Helena, fui desenvolvida para te auxiliar em atividades do seu cotidiano. Atualmente possuo recursos limitados, mas continuo em constante aperfeiçoamento. Bom uso!"
 
-msg = "Olá Eduardo, tudo bem?O que deseja acessar?"
+#msg = "Olá Eduardo, tudo bem?O que deseja acessar?"
 
-fala(msg)
+fala(bot)
+#fala(msg)
 voz = sr.Recognizer()
 
 with sr.Microphone() as s:
     while True:
         if estado==1:        
-            fala("Tchau, até a próxima Eduardo!")
+            fala("Tchau. Até a próxima!")
             sys.exit(1)
         try:
             print("Comando: ")
--- dependencias.sh
+++ dependencias.sh
@@ -4,3 +4,7 @@ pip3 install pyttsx3
 pip3 install pyaudio
 pip3 install pyobjc
 pip3 install pypi
+python3 -m pip install Pillow
+pip install pytesseract
+pip install opencv-python
+brew install tesseract
